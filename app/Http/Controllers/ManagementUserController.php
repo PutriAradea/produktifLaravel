37310eccc7bcc92cdfa115b02f44c25400e06320
PutriAradea <putriaradea777@gmail.com>
2025-02-27 14:18:14 +0700

    Pembaruan Controller

--- app/Http/Controllers/ManagementUserController.php
+++ app/Http/Controllers/ManagementUserController.php
@@ -3,83 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
 class ManagementUserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        // return "Halo ini adalah method index, dalam controller ManagementUser.";
+        // return "Method ini nantinya akan digunakan untuk mengambil semua data user";
+
+        $nama = "Arvita Agus Kurniasari";
+        $pelajaran = ["Algoritma & Pemrograman", "Kalkulus", "Pemrograman Web"];
+        return view('home', compact('nama', 'pelajaran'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk menambah data user";
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        return "Method ini nantinya akan digunakan untuk menciptakan data user yang baru";
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        return "Method ini nantinya akan digunakan untuk mengambil satu data user dengan id=" . $id;
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk mengubah data edit dengan id=" . $id;
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        return "Method ini nantinya akan digunakan untuk mengubah data user dengan id=" . $id;
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        return "Method ini nantinya akan digunakan untuk menghapus data user dengan id=" . $id;
     }
 }
